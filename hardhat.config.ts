8b7eea4a3439a58a972e33bfcc59f644a2ef16fc
Egor Polyakov <poegva@mail.ru>
2024-11-17 03:57:09 +0700

    Chain configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -28,16 +28,24 @@ const config: HardhatUserConfig = {
     ],
   },
   networks: {
-    mainnet: networkConfig(
-      `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-    ),
     sepolia: networkConfig(
       `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
     ),
+    polygonAmoy: networkConfig("https://rpc.ankr.com/polygon_amoy"),
+    arbitrumSepolia: networkConfig("https://rpc.ankr.com/arbitrum_sepolia"),
+    baseSepolia: networkConfig("https://rpc.ankr.com/base_sepolia"),
+    celoAlfajores: networkConfig("https://alfajores-forno.celo-testnet.org"),
+    scrollSepolia: networkConfig("https://rpc.ankr.com/scroll_sepolia_testnet"),
+    linea: networkConfig("https://rpc.linea.build"), // Need
+    hederaTestnet: networkConfig("https://testnet.hashio.io/api"),
+    flowTestnet: networkConfig("https://testnet.evm.nodes.onflow.org"),
+    mantleSepolia: networkConfig("https://rpc.sepolia.mantle.xyz"), // Need
+    kinto: networkConfig("https://rpc.kinto.xyz/http"), // Need
+    neonDevnet: networkConfig("https://devnet.neonevm.org"),
+    bitkubTestnet: networkConfig("https://rpc-testnet.bitkubchain.io"), // Need
   },
   etherscan: {
     apiKey: {
-      mainnet: process.env.ETHSCAN_API_KEY ?? "",
       sepolia: process.env.ETHSCAN_API_KEY ?? "",
     },
   },
--- scripts/configs.ts
+++ scripts/configs.ts
@@ -6,7 +6,7 @@ type NetworkConfig = {
       ethFeed?: string;
     };
     pyth: {
-      enabled: true;
+      enabled: boolean;
       address?: string;
     };
   };
@@ -22,6 +22,129 @@ export const Config: NetworkConfig = {
     pyth: {
       enabled: true,
       address: "0xDd24F84d36BF92C65F92307595335bdFab5Bbd21",
+    },
+  },
+  polygonAmoy: {
+    chainlink: {
+      enabled: true,
+      btcFeed: "0xe7656e23fE8077D438aEfbec2fAbDf2D8e070C4f",
+      ethFeed: "0xF0d50568e3A7e8259E16663972b11910F89BD8e7",
+    },
+    pyth: {
+      enabled: true,
+      address: "0x2880aB155794e7179c9eE2e38200202908C17B43",
+    },
+  },
+  arbitrumSepolia: {
+    chainlink: {
+      enabled: true,
+      btcFeed: "0x56a43EB56Da12C0dc1D972ACb089c06a5dEF8e69",
+      ethFeed: "0xd30e2101a97dcbAeBCBC04F14C3f624E67A35165",
+    },
+    pyth: {
+      enabled: true,
+      address: "0x4374e5a8b9C22271E9EB878A2AA31DE97DF15DAF",
+    },
+  },
+  baseSepolia: {
+    chainlink: {
+      enabled: true,
+      btcFeed: "0x0FB99723Aee6f420beAD13e6bBB79b7E6F034298",
+      ethFeed: "0x4aDC67696bA383F43DD60A9e78F2C97Fbbfc7cb1",
+    },
+    pyth: {
+      enabled: true,
+      address: "0xA2aa501b19aff244D90cc15a4Cf739D2725B5729",
+    },
+  },
+  celoAlfajores: {
+    chainlink: {
+      enabled: true,
+      btcFeed: "0xC0f1567a0037383068B1b269A81B07e76f99710c",
+      ethFeed: "0x7b298DA61482cC1b0596eFdb1dAf02C246352cD8",
+    },
+    pyth: {
+      enabled: true,
+      address: "0x74f09cb3c7e2A01865f424FD14F6dc9A14E3e94E",
+    },
+  },
+  scrollSepolia: {
+    chainlink: {
+      enabled: true,
+      btcFeed: "0x87dce67002e66C17BC0d723Fe20D736b80CAaFda",
+      ethFeed: "0x59F1ec1f10bD7eD9B938431086bC1D9e233ECf41",
+    },
+    pyth: {
+      enabled: true,
+      address: "0x41c9e39574F40Ad34c79f1C99B66A45eFB830d4c",
+    },
+  },
+  linea: {
+    chainlink: {
+      enabled: true,
+      btcFeed: "0x7A99092816C8BD5ec8ba229e3a6E6Da1E628E1F9",
+      ethFeed: "0x3c6Cd9Cc7c7a4c2Cf5a82734CD249D7D593354dA",
+    },
+    pyth: {
+      enabled: true,
+      address: "0xA2aa501b19aff244D90cc15a4Cf739D2725B5729",
+    },
+  },
+  hederaTestnet: {
+    chainlink: {
+      enabled: true,
+      btcFeed: "0x058fE79CB5775d4b167920Ca6036B824805A9ABd",
+      ethFeed: "0xb9d461e0b962aF219866aDfA7DD19C52bB9871b9",
+    },
+    pyth: {
+      enabled: true,
+      address: "0xa2aa501b19aff244d90cc15a4cf739d2725b5729",
+    },
+  },
+  flowTestnet: {
+    chainlink: {
+      enabled: false,
+    },
+    pyth: {
+      enabled: true,
+      address: "0x2880aB155794e7179c9eE2e38200202908C17B43",
+    },
+  },
+  mantleSepolia: {
+    chainlink: {
+      enabled: false,
+    },
+    pyth: {
+      enabled: true,
+      address: "0x98046Bd286715D3B0BC227Dd7a956b83D8978603",
+    },
+  },
+  kinto: {
+    chainlink: {
+      enabled: false,
+    },
+    pyth: {
+      enabled: true,
+      address: "0x2880aB155794e7179c9eE2e38200202908C17B43",
+    },
+  },
+  neonDevnet: {
+    chainlink: {
+      enabled: false,
+    },
+    pyth: {
+      enabled: true,
+      address: "0x0708325268dF9F66270F1401206434524814508b",
+    },
+  },
+  bitkubTestnet: {
+    chainlink: {
+      enabled: true,
+      btcFeed: "0xFDd3A5C3E27506F942139EE858EC61a7174681ed",
+      ethFeed: "0xDA1891Cc504B104B69fCF9DCBB855d0c77098BcB",
+    },
+    pyth: {
+      enabled: false,
     },
   },
 };
